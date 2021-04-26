b023842c4da338eb8ca80ee381eefe1c9969e712
Kay Mueller <mueller@fs-net.de>
2021-04-26 08:02:01 +0000

    Add check_config
    check_config read the configuration and determines, whether a device is
    present or not.

---
+++ cmd/fus_selftest/check_config.h
@@ -0,0 +1,16 @@
+/*
+ * check_config.h
+ *
+ *  Created on: Mar 08, 2021
+ *      Author: developer
+ */
+
+#ifndef CMD_FUS_SELFTEST_CHECK_CONFIG_H_
+#define CMD_FUS_SELFTEST_CHECK_CONFIG_H_
+
+int audio_present(void);
+int can_present(void);
+int sec_present(void);
+int wlan_present(void);
+
+#endif /* CMD_FUS_SELFTEST_CHECK_CONFIG_H_ */
