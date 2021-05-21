ecb03387e6582fc9e8f5eba3dbc40b85bbbe49eb
Philipp Gerbach <gerbach@fs-net.de>
2021-05-21 14:09:33 +0000

    Add RTS/CTS test to serial test
    the serial test now uses the gpio test to test RTS/CTS if present.

--- cmd/fus_selftest/gpio_test.h
+++ cmd/fus_selftest/gpio_test.h
@@ -4,5 +4,6 @@
 //---Functions for selftest---//
 
 int test_gpio(int uclass, char *szStrBuffer);
+int test_gpio_dev(struct udevice *dev, u32 *failmask); // return count of gpio pairs
 
 #endif // CMD_FUS_SELFTEST_GPIO_TEST_H_
