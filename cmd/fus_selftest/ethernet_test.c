567b3c25b25478e91073dfa47b7d1bd8d87bca73
Kay Mueller <mueller@fs-net.de>
2021-11-15 12:07:05 +0100

    Add DHCP request for PHY ethernet test

--- cmd/fus_selftest/ethernet_test.c
+++ cmd/fus_selftest/ethernet_test.c
@@ -24,7 +24,7 @@
 
 // main functions
 
-static void fus_reset_phy(void){
+static void fus_reset_phy(void) {
 	struct udevice *dev;
 	struct uclass *uc;
 	struct gpio_desc *desc;
@@ -56,6 +56,35 @@ static void fus_reset_phy(void){
 
 }
 
+static int get_i2c_bus(void) {
+	int i2c_bus;
+	unsigned int board_type = fs_board_get_type();
+
+	switch (board_type) {
+		case BT_PICOCOREMX8MX:
+			i2c_bus = 4;
+			break;
+		default:
+			i2c_bus = -1;
+			break;
+	}
+
+	return i2c_bus;
+}
+
+static void get_switch_name(char * name) {
+	unsigned int board_type = fs_board_get_type();
+
+	switch (board_type) {
+		case BT_PICOCOREMX8MX:
+			strcpy(name,"KSZ9893R");
+			break;
+		default:
+			strcpy(name,"NONE");
+			break;
+	}
+}
+
 /*
 commands:
 	- DHCP
@@ -67,7 +96,7 @@ int test_ethernet(char *szStrBuffer/*, char cmd*/)
 {
 	struct udevice *dev;
 	const void *fdt = gd->fdt_blob;
-	unsigned int board_type = fs_board_get_type();
+	
 	int node;
 	int port;
 
@@ -79,185 +108,164 @@ int test_ethernet(char *szStrBuffer/*, char cmd*/)
 	while (uclass_get_device(UCLASS_ETH,port,&dev) == 0) {
 		char * ethaddr = "";
 		int ret = 0, target_speed = 0;
+		bool fixed_link;
+		struct phy_device *phy;
+		int i2c_bus = get_i2c_bus();
+		char switch_name[32];
+		char ip[22];
+		char ip2[22];
+		bool port_failed = 0;
+		bool port2_failed = 0;
+
+		/* Suppress message when no mdio is found */
+		mute_debug_port(1);
+		phy = mdio_phydev_for_ethname(dev->name);
+		mute_debug_port(0);
 
 		node = dev_of_offset(dev);
 
 		node = fdt_subnode_offset(fdt, node, "fixed-link");
 
-		if (!strcmp("fixed-link",fdt_get_name(fdt, node, NULL))) {
-			int i2c_bus = 4;
-			char ip[22];
-			char ip2[22];
-			bool port_failed = 0;
-			bool port2_failed = 0;
+		fixed_link = !strcmp("fixed-link",fdt_get_name(fdt, node, NULL));
 
-			if (port == 0)
-				ethaddr = env_get("ethaddr");
+		if (port == 0)
+			ethaddr = env_get("ethaddr");
+		else
+			ethaddr = env_get("eth1addr");
+
+		printf("ETHERNET%d: ", port);
+
+		printf("MAC: %s\n", ethaddr);
+
+		port++;
+
+		printf("  internal loopback...");
+
+		if (fixed_link)
+		{
+			ret = ksz9893r_power_port(i2c_bus,0x1);
+			get_switch_name(switch_name);
+
+			if (ret) {
+				/* Switch doesn't communicate or is not present */
+				sprintf(szStrBuffer, "Switch timeout!");
+				test_OkOrFail(-1, 1, szStrBuffer);
+				continue;
+			}
 			else
-				ethaddr = env_get("eth1addr");
-
-			printf("ETHERNET%d: ", port);
-
-			printf("MAC: %s\n", ethaddr);
-
-			port++;
-
-			printf("  internal loopback...");
-
-			sprintf(szStrBuffer, "Fixed link");
-			test_OkOrFail(1, 1, szStrBuffer);
-
-			mute_debug_port(1);
-
-			env_set("autoload", "no");
-			env_set("bootpretryperiod", "10000");
-
-			if (board_type == BT_PICOCOREMX8MX) {
-
-				ksz9893r_power_port(i2c_bus,0x1);
-				net_loop(DHCP);
-				if(net_ip.s_addr)
-					ip_to_string(net_ip, ip);
-				else
-					port_failed = 1;
-
-				ksz9893r_power_port(i2c_bus,0x2);
-				net_loop(DHCP);
-				if(net_ip.s_addr)
-					ip_to_string(net_ip, ip2);
-				else
-					port2_failed = 1;
-			}
-			else {
-				net_loop(DHCP);
-				if(net_ip.s_addr)
-					ip_to_string(net_ip, ip);
-				else
-					port_failed = 1;
-			}
-
-//			net_loop(PING/TFTP);
-
-			mute_debug_port(0);
-
-			if (board_type == BT_PICOCOREMX8MX) {
-
-				/* ETH_A */
-				printf("  external loopback...");
-				if (!port_failed) {
-					sprintf(szStrBuffer, "ETH_A IP: %s", ip);
-					test_OkOrFail(0, 1, szStrBuffer);
-				}
-				else {
-					sprintf(szStrBuffer, "ETH_A failed with timeout");
-					test_OkOrFail(-1, 1, szStrBuffer);
-				}
-
-				/* ETH_B */
-				printf("  external loopback...");
-				if (!port2_failed) {
-					sprintf(szStrBuffer, "ETH_B IP: %s", ip2);
-					test_OkOrFail(0, 1, szStrBuffer);
-				}
-				else {
-					sprintf(szStrBuffer, "ETH_B failed with timeout");
-					test_OkOrFail(-1, 1, szStrBuffer);
-				}
-			}
-			else {
-
-				printf("  external loopback...");
-				if (!port_failed) {
-					sprintf(szStrBuffer, "IP: %s", ip);
-					test_OkOrFail(0, 1, szStrBuffer);
-				}
-				else {
-					sprintf(szStrBuffer, "Failed with timeout");
-					test_OkOrFail(-1, 1, szStrBuffer);
-				}
-			}
+				sprintf(szStrBuffer, switch_name);
 		}
-		else {
-			struct phy_device *phy = mdio_phydev_for_ethname(dev->name);
-
-
-			if (port == 0)
-				ethaddr = env_get("ethaddr");
-			else
-				ethaddr = env_get("eth1addr");
-
-			printf("ETHERNET%d: ", port);
-
-			printf("MAC: %s\n", ethaddr);
-
-			port++;
-
-			printf("  internal loopback...");
-
-			phy = mdio_phydev_for_ethname(dev->name);
-
+		else
+		{
 			fus_reset_phy();
 
 			mute_debug_port(1);
-
 			ret = phy_config(phy);
-
 			mute_debug_port(0);
 
-			if (ret)
-			{
+			if (ret) {
 				/* PHY doesn't communicate or is not present */
-			    sprintf(szStrBuffer, "PHY timeout!");
-			    test_OkOrFail(-1, 1, szStrBuffer);
+				sprintf(szStrBuffer, "PHY timeout!");
+				test_OkOrFail(-1, 1, szStrBuffer);
 				continue;
 			}
 
-			if (phy->drv->uid == 0xffffffff)
-			{
+			if (phy->drv->uid == 0xffffffff) {
 				/* No PHY found, generic driver is loaded */
-			    sprintf(szStrBuffer, "Can't match UID and PHY driver!");
-			    test_OkOrFail(-1, 1, szStrBuffer);
+				sprintf(szStrBuffer, "Can't match UID and PHY driver!");
+				test_OkOrFail(-1, 1, szStrBuffer);
 				continue;
 			}
 
-			test_OkOrFail(0, 1, szStrBuffer);
+			sprintf(szStrBuffer, "%s",phy->drv->name);
+		}
 
-			printf("  external loopback...");
+		test_OkOrFail(0, 1, szStrBuffer);
 
+		printf("  external loopback...");
+
+		if (!fixed_link)
+		{
 			mute_debug_port(1);
-
 			ret = phy_startup(phy);
-
 			mute_debug_port(0);
 
-			if (ret)
-			{
-			    sprintf(szStrBuffer, "Link up timeout!");
-			    test_OkOrFail(-1, 1, szStrBuffer);
+			if (ret) {
+				sprintf(szStrBuffer, "Link up timeout!");
+				test_OkOrFail(-1, 1, szStrBuffer);
 				continue;
 			}
+		}
 
+		env_set("autoload", "no");
+		env_set("bootpretryperiod", "10000");
+
+		mute_debug_port(1);
+		net_loop(DHCP);
+		if(net_ip.s_addr)
+			ip_to_string(net_ip, ip);
+		else
+			port_failed = 1;
+		mute_debug_port(0);
+
+		if (fixed_link)
+		{
+			ksz9893r_power_port(i2c_bus,0x2);
+			mute_debug_port(1);
+			net_loop(DHCP);
+			if(net_ip.s_addr)
+				ip_to_string(net_ip, ip2);
+			else
+				port2_failed = 1;
+			mute_debug_port(0);
+
+			/* ETH_A */
+			if (!port_failed)
+				sprintf(szStrBuffer, "ETH_A IP: %s", ip);
+			else
+				sprintf(szStrBuffer, "ETH_A timeout");
+
+			/* ETH_B */
+			if (!port2_failed)
+				sprintf(szStrBuffer + strlen(szStrBuffer), ", ETH_B IP: %s", ip2);
+			else
+				sprintf(szStrBuffer + strlen(szStrBuffer), ", ETH_B timeout");
+
+			if (port_failed || port2_failed) {
+				test_OkOrFail(-1, 1, szStrBuffer);
+				continue;
+			}
+		}
+		else
+		{
 			if (phy->drv->features & PHY_1000BT_FEATURES)
 				target_speed = 1000;
 			else if (phy->drv->features & PHY_100BT_FEATURES)
 				target_speed = 100;
 			else if (phy->drv->features & PHY_10BT_FEATURES)
 				target_speed = 10;
-			else
-			{
+			else {
 			    sprintf(szStrBuffer, "Unable to get Driver Speed");
 			    test_OkOrFail(-1, 1, szStrBuffer);
 				continue;
 			}
 
 			sprintf(szStrBuffer, "Connected with %dMbps", phy->speed);
-			if (phy->speed != target_speed)
-			{
+			if (phy->speed != target_speed) {
 			    test_OkOrFail(-1, 1, szStrBuffer);
 				continue;
 			}
-			test_OkOrFail(0, 1, szStrBuffer);
+
+			if (port_failed) {
+				sprintf(szStrBuffer + strlen(szStrBuffer), ", Loopback failed");
+				test_OkOrFail(-1, 1, szStrBuffer);
+				continue;
+			}
+			else
+				sprintf(szStrBuffer + strlen(szStrBuffer), ", IP: %s", ip);
 		}
+
+		test_OkOrFail(0, 1, szStrBuffer);
 	}
 
 	return 0;
