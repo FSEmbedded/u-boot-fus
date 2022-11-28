b8c395ec0cb7921ba9cca1ef631d7eb1378eb43a
Claudio Senatore <senatore@fs-net.de>
2022-11-28 12:20:54 +0100

    Edit Board Configuration
    Edit fsgal.c board file for GAL1 Board:
    Here, all unused functions for LS1028ARDB have been removed or simplified.
     - All QIXIS-APIs have been removed.
    
    Edit configs/fsgal_tfa_defconfig:
    Here, all Defines are configured for GAL1 Board
     - UBoot-memtest removed
     - set env_is_in_mmc
     - edit env_offset
     - edit default_device_tree
     - edit bootdelay
     - edit default bootargs
     - set mmc_write
     - set support_emmc_boot
     - remove i2c_mux
     - remove spi_flash
     - set phy_realtek/phy_mscc
     - remove nvme support
     - set rtc_pcf8563 driver
     - remove scsi support
     - remove video support
     - remove usb_ether support

--- board/F+S/fsgal/fsgal.c
+++ board/F+S/fsgal/fsgal.c
@@ -27,40 +27,12 @@
 
 #include <fdtdec.h>
 #include <miiphy.h>
-#include "../common/qixis.h"
 #include "../drivers/net/fsl_enetc.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int config_board_mux(void)
 {
-#ifndef CONFIG_LPUART
-#if defined(CONFIG_TARGET_LS1028AQDS) && defined(CONFIG_FSL_QIXIS)
-	u8 reg;
-
-	reg = QIXIS_READ(brdcfg[13]);
-	/* Field| Function
-	 * 7-6  | Controls I2C3 routing (net CFG_MUX_I2C3):
-	 * I2C3 | 10= Routes {SCL, SDA} to CAN1 transceiver as {TX, RX}.
-	 * 5-4  | Controls I2C4 routing (net CFG_MUX_I2C4):
-	 * I2C4 |11= Routes {SCL, SDA} to CAN2 transceiver as {TX, RX}.
-	 */
-	reg &= ~(0xf0);
-	reg |= 0xb0;
-	QIXIS_WRITE(brdcfg[13], reg);
-
-	reg = QIXIS_READ(brdcfg[15]);
-	/* Field| Function
-	 * 7    | Controls the CAN1 transceiver (net CFG_CAN1_STBY):
-	 * CAN1 | 0= CAN #1 transceiver enabled
-	 * 6    | Controls the CAN2 transceiver (net CFG_CAN2_STBY):
-	 * CAN2 | 0= CAN #2 transceiver enabled
-	 */
-	reg &= ~(0xc0);
-	QIXIS_WRITE(brdcfg[15], reg);
-#endif
-#endif
-
 	return 0;
 }
 
@@ -84,32 +56,6 @@ int board_init(void)
 #ifndef CONFIG_SYS_EARLY_PCI_INIT
 	pci_init();
 #endif
-
-#if defined(CONFIG_TARGET_LS1028ARDB)
-	u8 val = I2C_MUX_CH_DEFAULT;
-
-#if !CONFIG_IS_ENABLED(DM_I2C)
-	i2c_write(I2C_MUX_PCA_ADDR_PRI, 0x0b, 1, &val, 1);
-#else
-	struct udevice *dev;
-
-	if (!i2c_get_chip_for_busnum(0, I2C_MUX_PCA_ADDR_PRI, 1, &dev))
-		dm_i2c_write(dev, 0x0b, &val, 1);
-#endif
-#endif
-
-#if defined(CONFIG_TARGET_LS1028ARDB)
-	u8 reg;
-
-	reg = QIXIS_READ(brdcfg[4]);
-	/*
-	 * Field | Function
-	 * 3     | DisplayPort Power Enable (net DP_PWR_EN):
-	 * DPPWR | 0= DP_PWR is enabled.
-	 */
-	reg &= ~(DP_PWD_EN_DEFAULT_MASK);
-	QIXIS_WRITE(brdcfg[4], reg);
-#endif
 	return 0;
 }
 
@@ -129,32 +75,11 @@ int misc_init_r(void)
 
 int board_early_init_f(void)
 {
-#ifdef CONFIG_LPUART
-	u8 uart;
-#endif
-
 #ifdef CONFIG_SYS_I2C_EARLY_INIT
 	i2c_early_init_f();
 #endif
 
 	fsl_lsch3_early_init_f();
-
-#ifdef CONFIG_LPUART
-	/*
-	 * Field| Function
-	 * --------------------------------------------------------------
-	 * 7-6  | Controls I2C3 routing (net CFG_MUX_I2C3):
-	 * I2C3 | 11= Routes {SCL, SDA} to LPUART1 header as {SOUT, SIN}.
-	 * --------------------------------------------------------------
-	 * 5-4  | Controls I2C4 routing (net CFG_MUX_I2C4):
-	 * I2C4 |11= Routes {SCL, SDA} to LPUART1 header as {CTS_B, RTS_B}.
-	 */
-	/* use lpuart0 as system console */
-	uart = QIXIS_READ(brdcfg[13]);
-	uart &= ~CFG_LPUART_MUX_MASK;
-	uart |= CFG_LPUART_EN;
-	QIXIS_WRITE(brdcfg[13], uart);
-#endif
 
 	return 0;
 }
@@ -242,46 +167,12 @@ int ft_board_setup(void *blob, struct bd_info *bd)
 }
 #endif
 
-#ifdef CONFIG_FSL_QIXIS
 int checkboard(void)
 {
 #ifdef CONFIG_TFABOOT
 	enum boot_src src = get_boot_src();
 #endif
-	u8 sw;
-
-	int clock;
-	char *board;
-	char buf[64] = {0};
-	static const char *freq[6] = {"100.00", "125.00", "156.25",
-					"161.13", "322.26", "100.00 SS"};
-
-	cpu_name(buf);
-	/* find the board details */
-	sw = QIXIS_READ(id);
-
-	switch (sw) {
-	case 0x46:
-		board = "QDS";
-		break;
-	case 0x47:
-		board = "RDB";
-		break;
-	case 0x49:
-		board = "HSSI";
-		break;
-	default:
-		board = "unknown";
-		break;
-	}
-
-	sw = QIXIS_READ(arch);
-	printf("Board: %s-%s, Version: %c, boot from ",
-	       buf, board, (sw & 0xf) + 'A' - 1);
-
-	sw = QIXIS_READ(brdcfg[0]);
-	sw = (sw & QIXIS_LBMAP_MASK) >> QIXIS_LBMAP_SHIFT;
-
+	puts("BOOT-SRC: ");
 #ifdef CONFIG_TFABOOT
 	if (src == BOOT_SOURCE_SD_MMC) {
 		puts("SD\n");
@@ -289,49 +180,18 @@ int checkboard(void)
 		puts("eMMC\n");
 	} else {
 #endif
-#ifdef CONFIG_SD_BOOT
+#if defined(CONFIG_SD_BOOT)
 		puts("SD\n");
 #elif defined(CONFIG_EMMC_BOOT)
 		puts("eMMC\n");
 #else
-		switch (sw) {
-		case 0:
-		case 4:
-			printf("NOR\n");
-			break;
-		case 1:
-			printf("NAND\n");
-			break;
-		default:
-			printf("invalid setting of SW%u\n", QIXIS_LBMAP_SWITCH);
-			break;
-		}
+		puts("unknown\n");
 #endif
 #ifdef CONFIG_TFABOOT
 	}
 #endif
-
-	printf("FPGA: v%d (%s)\n", QIXIS_READ(scver), board);
-	puts("SERDES1 Reference : ");
-
-	sw = QIXIS_READ(brdcfg[2]);
-#ifdef CONFIG_TARGET_LS1028ARDB
-	clock = (sw >> 6) & 3;
-#else
-	clock = (sw >> 4) & 0xf;
-#endif
-
-	printf("Clock1 = %sMHz ", freq[clock]);
-#ifdef CONFIG_TARGET_LS1028ARDB
-	clock = (sw >> 4) & 3;
-#else
-	clock = sw & 0xf;
-#endif
-	printf("Clock2 = %sMHz\n", freq[clock]);
-
 	return 0;
 }
-#endif
 
 void *video_hw_init(void)
 {
