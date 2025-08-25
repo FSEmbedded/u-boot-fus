c6a35e5f6a2d3106986248e84c1addc43eef681f
Claudio Senatore <senatore@fs-net.de>
2025-08-25 11:00:11 +0200

    configs:fsimx8ulp: update default env
    Update default U-Boot environment.
    Use FUS_BOOTCMD environment for default bootflow.

--- include/configs/fsimx8ulp.h
+++ include/configs/fsimx8ulp.h
@@ -57,140 +57,44 @@
 #define CFG_SYS_UBOOT_BASE \
 	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
 
-// Gets overridden by UBoot if the Board is closed
-#define AHAB_ENV "sec_boot=no\0"
-#define BOOT_OS "boot_os=booti ${loadaddr} - ${fdt_addr_r};\0"
-#define BOOT_OS_FIT "boot_os_fit=bootm 0x84000000#conf-${fdtfile};\0"
-
-#if defined(CONFIG_MTDIDS_DEFAULT)
-#define CFG_MTDPART_DEFAULT ""
-#endif
-
 /* ENET Config */
 #if defined(CONFIG_FEC_MXC)
 #define PHY_ANEG_TIMEOUT		20000
+#define CONFIG_FEC_XCV_TYPE             RMII
+#define FEC_QUIRK_ENET_MAC
 #endif
 
-#ifdef CONFIG_DISTRO_DEFAULTS
-#define BOOT_TARGET_DEVICES(func) \
-	func(USB, usb, 0) \
-	func(MMC, mmc, 0)
-
-#include <config_distro_bootcmd.h>
-#else
-#define BOOTENV
-#endif
-
-#define CFG_MFG_ENV_SETTINGS \
-	CFG_MFG_ENV_SETTINGS_DEFAULT \
-	"initrd_addr=0x83800000\0" \
-	"initrd_high=0xffffffffffffffff\0" \
-	"emmc_dev=0\0"\
-	"sd_dev=2\0"
-
+#include <config_fus_bootcmd.h>
 /* Initial environment variables */
 #define CFG_EXTRA_ENV_SETTINGS		\
-	CFG_MFG_ENV_SETTINGS \
-	BOOTENV \
-	AHAB_ENV \
-	"arch=" CONFIG_SYS_BOARD "\0" \
-	"scriptaddr=0x83500000\0" \
-	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
-	"image=Image\0" \
-	"splashimage=0x90000000\0" \
-	"console=undef\0" \
-	".console_none=setenv console\0"				\
-	".console_serial=setenv console ${sercon},${baudrate}\0" \
-	".console_display=setenv console tty1\0"		\
-	"fdt_addr_r=0x83000000\0"			\
-	"fdt_addr=0x83000000\0"			\
-	"fdt_high=0xffffffffffffffff\0"		\
-	"cntr_addr=0x98000000\0"			\
-	"cntr_file=os_cntr_signed.cntr\0" \
-	"boot_fit=no\0" \
-	"fdtfile=undef\0" \
-	"set_bootfdt=setenv fdtfile ${platform}.dtb\0"			\
-	"bootm_size=0x10000000\0" \
-	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
-	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk0p2 rootwait rw\0" \
-	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} console=${console} root=${mmcroot}\0 " \
-	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
-	"bootscript=echo Running bootscript from mmc ...; " \
-		"source\0" \
-	"loadimage=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}\0" \
-	"loadfdt=fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${fdtfile}\0" \
-	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
-	"auth_os=auth_cntr ${cntr_addr}\0" \
-	BOOT_OS \
-	BOOT_OS_FIT \
-	"mmcboot=echo Booting from mmc ...; " \
-		"run mmcargs; " \
-		"if test ${sec_boot} = yes; then " \
-			"if run auth_os; then " \
-				"run boot_os_fit; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
-		"else " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if run loadfdt; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
-		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} console=${console} " \
-		"root=/dev/nfs " \
-		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
-	"netboot=echo Booting from net ...; " \
-		"run netargs;  " \
-		"if test ${ip_dyn} = yes; then " \
-			"setenv get_cmd dhcp; " \
-		"else " \
-			"setenv get_cmd tftp; " \
-		"fi; " \
-		"if test ${sec_boot} = yes; then " \
-			"${get_cmd} ${cntr_addr} ${cntr_file}; " \
-			"if run auth_os; then " \
-				"run boot_os_fit; " \
-			"else " \
-				"echo ERR: failed to authenticate; " \
-			"fi; " \
-		"else " \
-			"${get_cmd} ${loadaddr} ${image}; " \
-			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
-				"bootm ${loadaddr}; " \
-			"else " \
-				"if ${get_cmd} ${fdt_addr_r} ${fdtfile}; then " \
-					"run boot_os; " \
-				"else " \
-					"echo WARN: Cannot load the DT; " \
-				"fi; " \
-			"fi;" \
-		"fi;\0" \
-	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
-		"mmc dev ${mmcdev}; " \
-		"if mmc rescan; then " \
-			"if test ${sec_boot} = yes; then " \
-				"if run loadsignedscript; then " \
-		  			"run signedscript; " \
-	   			"fi; " \
-		   		"if run loadcntr; then " \
-			   		"run mmcboot; " \
-		   		"else run netboot; " \
-		   		"fi; " \
-		   	"else " \
-		   		"if run loadimage; then " \
-			   		"run mmcboot; " \
-		   		"else run netboot; " \
-		   		"fi; " \
-			"fi; " \
-		"fi;"
+	"arch=" CONFIG_SYS_BOARD "\0" 						\
+	"script=boot_script.scr\0"						\
+	"scriptaddr=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" 			\
+	"kernel_addr_r=0x80400000\0"						\
+	"fdt_addr_r=0x83000000\0"						\
+	"fdt_addr=0x83000000\0"							\
+	"fdt_high=0xffffffffffffffff\0"						\
+	"set_bootfdt=setenv fdtfile ${platform}.dtb\0"				\
+	"fdtfile=undef\0" 							\
+	"cntr_addr_r=0x98000000\0"						\
+	"bootcntrfile=os_cntr_signed.cntr\0" 					\
+	"splashimage=0x90000000\0" 						\
+	"bootfile=Image\0" 							\
+	"prepare_mcore=setenv mcore_clk clk-imx93.mcore_booted;\0" 		\
+	"bootm_size=0x10000000\0" 						\
+	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" 			\
+	"prefix=/\0"								\
+	"updatecheck=undef\0"							\
+	"installcheck=undef\0"							\
+	"recovercheck=undef\0"							\
+	FUS_BOOT_ENV								\
+	FUS_AHAB_ENV								\
+	FUS_AB_BOOT								\
+	FUS_LEGACY_BOOT								\
+	".default_boot=setenv boot_targets fus_legacy "				\
+		"mmc0 mmc1 usb0 usb1;\0"					\
+	"boot_targets=fus_legacy mmc0 mmc1 usb0 usb1\0"
+
 
 /* Link Definitions */
 
@@ -202,7 +106,7 @@
 #define CFG_SPL_FUS_EARLY_AHAB_BASE	CFG_SPL_ATF_ADDR
 #define PHYS_SDRAM			0x80000000
 
-/**
+/*
  * The real DRAM Size is determinied by BOARD-CFG.
  * PHYS_SDRAM_SIZE is used for xrdc configs and imx8ulp_arm64_mem_map.
  * The Value in imx8ulp_arm64_mem_map is overwritten during dram_init()
@@ -222,7 +126,6 @@
 /* Using ULP WDOG for reset */
 #define WDOG_BASE_ADDR			WDG3_RBASE
 
-// /* USB Configs */
-// #define CONFIG_USB_MAX_CONTROLLER_COUNT 2
-
+/* USB Configs */
+#define CONFIG_USB_MAX_CONTROLLER_COUNT 2
 #endif
